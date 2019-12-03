ed14ca5605514f8d4dc6f55337035ecc971521f5
ttanprasert <ttanprasert@hmc.edu>
2019-12-02 17:38:44 -0800

    Basic line graphs

---
+++ angular-src/shakespeer/src/app/components/relationship/relationship_data.ts
@@ -0,0 +1,28 @@
+export var sentimentAB = [
+    {scene: 1, sentiment_value: 22.0, speech_dist: 406},
+    {scene: 2, sentiment_value: 22.0, speech_dist: 0},
+    {scene: 3, sentiment_value: 22.0, speech_dist: 0},
+    {scene: 4, sentiment_value: 19.0, speech_dist: 359},
+    {scene: 5, sentiment_value: 19.0, speech_dist: 0},
+    {scene: 6, sentiment_value: -1.0, speech_dist: 477},
+    {scene: 7, sentiment_value: -1.0, speech_dist: 75},
+    {scene: 8, sentiment_value: -1.0, speech_dist: 0},
+    {scene: 9, sentiment_value: -1.0, speech_dist: 79}
+];
+
+export var sentimentBA = [
+    {scene: 1, sentiment_value: 7.0, speech_dist: 432},
+    {scene: 2, sentiment_value: 7.0, speech_dist: 0},
+    {scene: 3, sentiment_value: 7.0, speech_dist: 0},
+    {scene: 4, sentiment_value: 22.0, speech_dist: 205},
+    {scene: 5, sentiment_value: 22.0, speech_dist: 0},
+    {scene: 6, sentiment_value: 7.0, speech_dist: 633},
+    {scene: 7, sentiment_value: 7.0, speech_dist: 16},
+    {scene: 8, sentiment_value: 7.0, speech_dist: 0},
+    {scene: 9, sentiment_value: 7.0, speech_dist: 0}
+];
+
+export var characterPairs = [
+    {pair: "LYSANDER, HERMIA"},
+    {pair: "HERMIA, LYSANDER"}
+];
