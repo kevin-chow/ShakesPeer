89c6b41b01c545604c1ef37e68b45f21cf534fd9
Frances Sin <frances.sin31@gmail.com>
2019-12-06 14:10:10 -0800

    Node-link with weird links

--- angular-src/shakespeer/src/app/components/network/network.component.ts
+++ angular-src/shakespeer/src/app/components/network/network.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 import * as d3 from 'd3';
 import { nodes, links, type } from './speech_data';
-
+import { ResolveStart } from '@angular/router';
 
 @Component({
   selector: 'app-network',
@@ -13,35 +13,36 @@ export class NetworkComponent implements OnInit {
 
   maxWords = 2035;
   maxRadius = 50;
-  minRadius = 15;
-
-  title = 'ng-d3-graph-editor';
-  @ViewChild('graphContainer') graphContainer: ElementRef;
-
+  maxSentiment = 8;
+  minRadius = 17;
   width = 4000;
-  height = 700;
-  colors = d3.scaleOrdinal(d3.schemeSet3);
+  height = 600;
+  selectedScn = 'overview';
+
+  @ViewChild('graphContainer') graphContainer: ElementRef;
 
   svg: any;
   force: any;
   path: any;
   circle: any;
   drag: any;
-  dragLine: any;
+  brush: any;
 
-  selectedScn = 'overview';
-
+  // Colors
   posColor = d3.color('#80b1d3');
   negColor = d3.color('#fb8072');
   neutralColor = d3.color('#d9d9d9');
-
   fairyColor = d3.color('#b3de69');
   loverColor = d3.color('#bc80bd');
   actorColor = d3.color('#fdb462');
 
+  colors = d3.scaleLinear().domain([-1, 1])
+    .range([this.negColor, this.posColor]);
+
 
   // Mouse event variables
   selectedNode = null;
+  selectedNodePair = null;
  
 
   handleScnSelection(e) {
@@ -58,6 +59,9 @@ export class NetworkComponent implements OnInit {
 
   reset() {
     this.svg.selectAll("*").remove();
+    nodes.forEach(node => {
+      node.selected = false;
+    })
     if(this.selectedScn == 'overview') {
       this.maxWords = 2035;
     } else if (this.selectedScn == 'a1s1') {
@@ -125,17 +129,12 @@ export class NetworkComponent implements OnInit {
         .distance(function(d) {
           return d.source.radius + 60 + d.target.radius;
         }))
-      .force('charge', d3.forceManyBody().strength(function(d, i) {
-        if (i == 0) {
-          // return -10000;
-        } else {
-          return -105 * d.radius;
-        }
-      }))
-      .force('x', d3.forceX(this.width / 2).strength(0.25))
-      .force('y', d3.forceY(this.height / 2).strength(0.25))
+      .force('charge', d3.forceManyBody().strength((d) => (-105 * d.radius) ))
+      .force('x', d3.forceX(this.width / 2).strength(0.45))
+      .force('y', d3.forceY((this.height / 2)).strength(0.5))
       .on('tick', () => this.tick());
   }
+
 
   setSvg() {
     this.svg = d3.select('#graphContainer')
@@ -143,19 +142,36 @@ export class NetworkComponent implements OnInit {
       .attr('width', this.width)
       .attr('height', this.height);
 
+    // this.svg.append("defs").append("marker")
+    // .attr("id", "arrow")
+    // .attr("viewBox", "0 -5 10 10")
+    // .attr("refX", 9)
+    // .attr("refY", 0)
+    // .attr("markerWidth", 3)
+    // .attr("markerHeight", 3)
+    // .attr("orient", "auto")
+    // .append('svg:path') 
+    // .attr("d", "M 0, -5 L 10, 0 L 0, 5")
+    // .attr('fill', this.neutralColor);
+
     this.svg.append("defs").append("marker")
-      .attr("id", "arrow")
-      .attr("viewBox", "0 -5 10 10")
-      .attr("refX", 9)
-      .attr("refY", 0)
-      .attr("markerWidth", 3)
-      .attr("markerHeight", 3)
-      .attr("orient", "auto")
-      .append('svg:path') 
-      .attr("d", "M 0, -5 L 10, 0 L 0, 5")
-      //.attr('fill', d3.rgb("#bbb"));
-      .attr('fill', this.neutralColor);
-      
+    .attr("id", "pos_arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 9)
+    .attr("refY", 0)
+    .attr("markerWidth", 3)
+    .attr("markerHeight", 3)
+    .attr("orient", "auto")
+    .append('svg:path') 
+    .attr("d", "M 0, -5 L 10, 0 L 0, 5")
+    .attr('fill', this.posColor);
+
+  // this.svg.on('click', function() {
+  //   this.selectedNode = null;
+  //   d3.selectAll('circle')
+  //   .style('stroke', d3.rgb('white'));
+  // });
+  
   // Handles to link and node element groups
   this.path = this.svg.append('svg:g').selectAll('path');
   this.circle = this.svg.append('svg:g').selectAll('g');
@@ -166,10 +182,7 @@ export class NetworkComponent implements OnInit {
     const rect = this.graphContainer.nativeElement.getBoundingClientRect();
     this.width = rect.width;
 
-    
-
-  
-      this.drag = d3.drag()
+    this.drag = d3.drag()
       .on('start', (d: any) => {
         if (!d3.event.active) this.force.alphaTarget(0.2).restart();
         d.fx = d.x;
@@ -184,11 +197,7 @@ export class NetworkComponent implements OnInit {
         d.fx = null;
         d.fy = null;
       });
-
-      
-
-    
-    
+ 
     // App starts here
     this.setSvg();
     this.setRadius();
@@ -198,61 +207,73 @@ export class NetworkComponent implements OnInit {
 
   // Update force layout (called automatically each iteration)
   tick() {
-
-    // this.path.attr("d", function (d) {
-    //   var dx = d.target.x - d.source.x,
-    //       dy = d.target.y - d.source.y,
-    //       dr = Math.sqrt(dx * dx + dy * dy);
-      
-    //     var sourceX = d.source.x + (d.source.radius * (dx / dr)),
-    //       sourceY = d.source.y + (d.source.radius * (dy / dr)),
-    //       targetX = d.target.x - (d.target.radius * (dx / dr)),
-    //       targetY = d.target.y - (d.target.radius * (dy / dr));
-
-    //   return "M" + sourceX + "," + sourceY + "A" + dr + "," + dr + " 0 0,1 " + targetX + "," + targetY;
-    // });
-   
     this.path.attr('d', (d) => {
-      const deltaX = d.target.x - d.source.x;
-      const deltaY = d.target.y - d.source.y;
-      const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-      const normX = deltaX / dist;
-      const normY = deltaY / dist;
-      const sourcePadding = d.source.radius;
-      const targetPadding = d.target.radius;
-      const sourceX = d.source.x + (sourcePadding * normX);
-      const sourceY = d.source.y + (sourcePadding * normY);
-      const targetX = d.target.x - (targetPadding * normX);
-      const targetY = d.target.y - (targetPadding * normY);
-      return `M${sourceX},${sourceY}L${targetX},${targetY}`;
-    });
+      let offset = 5;
+
+      return `
+            M 
+              ${d.source.x} ${d.source.y} 
+            C 
+              ${(d.source.x + d.target.x) / 2} ${d.source.y} 
+              ${(d.source.x + d.target.x) / 2} ${d.target.y} 
+              ${d.target.x} ${d.target.y}
+            M 
+              ${d.source.x +offset} ${d.source.y+offset}
+            C
+            ${(d.source.x+offset + d.target.x) / 2} ${d.source.y+offset} 
+            ${(d.source.x+offset + d.target.x) / 2} ${d.target.y} 
+            ${d.target.x} ${d.target.y}
+
+            L
+            ${d.source.x} ${d.source.y}
+            ${d.source.x+offset} ${d.source.y+offset}
+            
+          `
+        });
+      // const controlPts = [(d.target)]
+
+
+      // const deltaX = d.target.x - d.source.x;
+      // const deltaY = d.target.y - d.source.y;
+      // const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+      // const normX = deltaX / dist;
+      // const normY = deltaY / dist;
+      // const sourcePadding = d.source.radius;
+      // const targetPadding = d.target.radius;
+    
+      // const sourceX = d.source.x + (sourcePadding * normX);
+      // const sourceY = d.source.y + (sourcePadding * normY);
+      // const targetX = d.target.x - (targetPadding * normX );
+      // const targetY = d.target.y - (targetPadding * normY);
+
+      // return `M${sourceX},${sourceY}L${targetX},${targetY}`;
+    // });
     this.circle.attr('transform', (d) => `translate(${d.x},${d.y})`);
   }
+  
+  handleSelect(d) {
+    nodes.forEach(n => {
+      n.selected = false;
+    });
+    d.selected = !d.selected;
+  }
 
-
-  // Update graph 
+ 
+  // Update graph
   restart() {
     this.path = this.path.data(links);
-    // Remove old links
     this.path.exit().remove();
 
     // Add new links
     this.path = this.path.enter().append('svg:path')
       .attr('class', 'link')
-      // .style('stroke', d3.rgb("#bbb"))
       .style('stroke', this.neutralColor)
       .style('stroke-width', '2px')
-      .style('fill', 'none')
-      .attr('marker-end','url(#arrow)')
+      // .attr('marker-end','url(#arrow)')
       .style('opacity', 0.75)
-      .style('display', function(d) {
-        if (d.source.visible) {
-          if (d.target.visible) {
-            return 'flex';
-          } 
-        }
-        return 'none';
-      });
+      .attr("fill", 'transparent')
+      .style('display', (d) => (d.source.visible) ? ((d.target.visible) ? 'flex' : 'none') : 'none');
+
 
     this.circle = this.circle.data(nodes, (d) => d.id);
     
@@ -262,26 +283,47 @@ export class NetworkComponent implements OnInit {
     // Add new nodes
     const g = this.circle.enter().append('svg:g');
 
+    var temp = this;
     g.append('svg:circle')
       .attr('class', 'node')
-      .attr('r', function(d) { return d.radius })
-      //.style('fill', (d) => this.colors(d.id))
-      //.style('fill', d3.rgb('#8dd3c7'))
-      // .style('fill', this.neutralColor)
+      .attr('r', (d) => d.radius)
       .style('fill', (d) => 
         (d.type == type.LOVER) ? this.loverColor : 
         (d.type == type.FAIRY) ? this.fairyColor : 
         (d.type == type.ACTOR) ? this.actorColor : this.neutralColor)
-      
-      .style('stroke', () => d3.rgb("white"))
-      .style('stroke-width', '2px')
-      .on('mouseover', function() {
-        d3.select(this).style('transform', 'scale(1.2)');
-      })
-      .on('mouseout', function() {
-        d3.select(this).style('transform', 'scale(1)');
+        .style('stroke-width', '2px')
+        .style('stroke', 'white')
+      .on('click', function(d) {
+        temp.handleSelect(d);
+        d3.selectAll('circle')
+          .style('stroke', (d) => (d.selected) ? temp.neutralColor : 'white');
+        d3.selectAll('path')
+          .style('stroke', function(nd) {
+            if (nd == undefined) {
+              return temp.neutralColor;
+            } else if ((nd.source.id == d.id || nd.target.id == d.id) && d.selected) {
+
+              return temp.colors(nd.sentiment);
+            }
+            else return temp.neutralColor;
+          
+              
+            //   if(nd.sentiment > 0) {
+            //     return temp.posColor;
+            //   } else if (nd.sentiment <= 0) {
+            //     return temp.negColor;
+            //   }
+            //   else {
+            //     return temp.neutralColor;
+            //   }
+           
+            // } else {
+            //   return temp.neutralColor;
+            // }
+          })
+        
       });
-      
+        
 
     g.append('svg:text')
       .attr('x', 0)
@@ -293,16 +335,9 @@ export class NetworkComponent implements OnInit {
       .style('display', (d) => (d.visible) ? 'flex' : 'none');
 
     this.circle = g.merge(this.circle)
+      
       .call(this.drag);
       
-     
-      // .on('mouseout', function(d) {
-      //   d3.select(this)
-      //   .style('opacity', 1.0);
-      // })
-      ;
-
-  
 
   
     // set the graph in motion
@@ -311,16 +346,14 @@ export class NetworkComponent implements OnInit {
       .force('link').links(links);
     
     this.force.alpha(1).restart();
-    
-    
 
- // }
 
   }
-//}
+
 
 
 
 
 
 }
+
