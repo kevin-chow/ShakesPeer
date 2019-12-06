e49db5494d691be1bb43b7e339ac332a96f16956
ttanprasert <ttanprasert@hmc.edu>
2019-12-06 14:18:03 -0800

    add data analysis and update sentiment data

--- angular-src/shakespeer/src/app/components/relationship/relationship.component.ts
+++ angular-src/shakespeer/src/app/components/relationship/relationship.component.ts
@@ -17,28 +17,36 @@ export class RelationshipComponent implements OnInit {
     private y: any;
     private svg: any;
     private line: d3.line<[number, number]>; // this is line definition
+    private counter = 0;
 
     sentimentAB: any;
     sentimentBA: any;
 
+    handleScnSelection(e) {
+    
+    var lexicon = "AFINN";
+    if (e.target.checked) {
+      var selectedRlt = e.target.id;
+      console.log(selectedRlt);
+      var ind = selectedRlt.indexOf("-");
+      var A = selectedRlt.slice(0, ind);
+      var B = selectedRlt.slice(ind+1);
+      this.buildSvg('#svg1', A, B, lexicon, this.counter);
+      this.counter = this.counter+1; 
+    }
+    
+  }
+
   constructor() { 
       // configure margins and width/height of the graph
-
    this.width = 350 - this.margin.left - this.margin.right;
    this.height = 200 - this.margin.top - this.margin.bottom;}
 
-  ngOnInit() {
-        var A = "THESEUS";
-        var B = "HIPPOLYTA";
-        var lexicon = "AFINN";
-        this.readData(A, B, lexicon);
-        this.buildSvg('#svg1', A, B);
+  ngOnInit() {}
 
-        A = "LYSANDER";
-        B = "HERMIA";
-        this.readData(A, B, lexicon);
-        this.buildSvg('#svg2', A, B);
-    }
+  ngAfterContentInit() {
+      
+  }
 
     private readData(A, B, lexicon) {
         if (lexicon == "AFINN") {
@@ -51,25 +59,35 @@ export class RelationshipComponent implements OnInit {
         }
     }
 
-    private buildSvg(svgID, A, B) {
-        d3.select(svgID).append("rect")
+    private buildSvg(svgID, A, B, lexicon, i) {
+        
+        this.readData(A, B, lexicon);
+
+        var y = i * 200;
+        this.svg = d3.select(svgID).append("rect")
             .attr("x", 0)
-            .attr("y", 0)
+            .attr("y", y)
             .attr("width", 350)
             .attr("height", 200)
             .attr("stroke", "black")
             .attr("stroke-width", 1)
-            .attr("fill", "none");
+            .attr("fill", "none")
+            .attr("id", A+"-"+B)
+            .on("click", this.removeGraph(A+"-"+B));
 
-        this.svg = d3.select(svgID)
-            .append('g')
+        var top = this.margin.top + y;
+        this.svg = d3.select(svgID).append('g')
             .attr('class', 'line-and-dots')
-            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+            .attr('transform', 'translate(' + this.margin.left + ',' + top + ')');
 
         this.addXandYAxis();
         this.drawLineAndPath(this.sentimentAB, 'blue', 'circle');
         this.drawLineAndPath(this.sentimentBA, 'red', 'rect');
         this.addLegend(A, B);
+    }
+
+    private removeGraph(id) {
+        d3.select(id).exit().remove();
     }
 
     private addXandYAxis() {
@@ -109,6 +127,21 @@ export class RelationshipComponent implements OnInit {
         }
     }
 
+    private mouseover = function(d) {
+          var g = d3.select(this); // The node
+          // The class is used to remove the additional text later
+          var info = g.append('text')
+             .classed('info', true)
+             .attr('x', 20)
+             .attr('y', 10)
+             .text(d);
+      };
+
+    private mouseout = function() {
+          // Remove the info text on mouse out.
+          d3.select(this).select('text.info').remove()
+      };
+
     private drawLineAndPath(data, color, shape) {
         this.line = d3.line()
             .x( (d: any) => this.x(d.scene) )
@@ -131,6 +164,8 @@ export class RelationshipComponent implements OnInit {
               .enter()
               .append(shape)
               .attr("class", "points")
+              .on("mouseover", this.mouseover)
+              .on("mouseout", this.mouseout)
               .attr("r", (d: any) => this.computePointSize(d.speech_dist))
               .attr("cx", (d: any) => this.x(d.scene))
               .attr("cy", (d: any) => this.y(d.sentiment_value) )
@@ -145,6 +180,8 @@ export class RelationshipComponent implements OnInit {
               .enter()
               .append(shape)
               .attr("class", "points")
+              .on("mouseover", (d: any) => this.mouseover(d.sentiment_value))
+              .on("mouseout", this.mouseout)
               .attr("x", (d: any) => this.x(d.scene)-this.computePointSize(d.speech_dist))
               .attr("y", (d: any) => this.y(d.sentiment_value)-this.computePointSize(d.speech_dist))
               .attr("width", (d: any) => 2*this.computePointSize(d.speech_dist))
