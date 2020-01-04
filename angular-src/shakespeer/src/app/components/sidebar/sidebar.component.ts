cef62614a50c3544129b90039bf1297e62464657
Kevin Chow <kevin.chow.c@gmail.com>
2020-01-04 20:23:02 +0800

    small fixes for deployment purposes

--- angular-src/shakespeer/src/app/components/sidebar/sidebar.component.ts
+++ angular-src/shakespeer/src/app/components/sidebar/sidebar.component.ts
@@ -14,6 +14,7 @@ export class SidebarComponent implements OnInit {
   characterTypeList: CharacterType[] = [];
   ascOrder: boolean = false;
   show: boolean = true;
+  showSummary: boolean = false;
 
   characterAppearList: any[] = allCharacters;
   characterList: any[] = allCharacters;
